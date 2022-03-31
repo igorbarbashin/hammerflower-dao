9b01aca6d20dbb3674b14259b5a6e971a0a97a43
Igor Barbashin <igor.barbashin@gmail.com>
2022-03-30 21:28:29 -0700

    Add testable mint function

--- contracts/HammerFlower.sol
+++ contracts/HammerFlower.sol
@@ -3,8 +3,28 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+import "hardhat/console.sol";
 
 contract HammerFlower is ERC721 {
-    constructor() ERC721("HammerFlower", "FLOWER") {
-    }
+  using Counters for Counters.Counter;
+  Counters.Counter private _tokenIds;
+
+  // A struct to store the flower power
+  mapping (uint256 => uint256) public powers;
+  
+
+  constructor() ERC721("HammerFlower", "FLOWER") {
+  }
+
+  function _mint() public payable returns (uint256) {
+    _tokenIds.increment();
+    uint256 newTokenId = _tokenIds.current();
+    powers[newTokenId] = msg.value;
+    super._mint(msg.sender, newTokenId);
+    console.log("New token ID", newTokenId);
+
+    return newTokenId;
+  }
 }
--- test/HammerFlower.test.ts
+++ test/HammerFlower.test.ts
@@ -2,11 +2,19 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("HammerFlower", function () {
-  it("Should mint a token", async () => {
+  it("Should work", async () => {
+    const [owner, addr1, addr2] = await ethers.getSigners();
+
     const HammerFlower = await ethers.getContractFactory("HammerFlower");
     const hammerFlower = await HammerFlower.deploy();
     await hammerFlower.deployed();
 
     expect(await hammerFlower.symbol()).to.equal("FLOWER");
+    await hammerFlower._mint();
+
+    expect(await hammerFlower.balanceOf(owner.address)).to.equal(1);
+    // Should mint with a payable mint function
+    // Need to be able to check the sum power
+    // Transfer token reduces power
   });
 });
