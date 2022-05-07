4adda6c2bb906cb46d265eb3b652cc96603bd70d
Igor Barbashin <igor.barbashin@gmail.com>
2022-05-07 10:16:53 -0700

    Add pseudocode for garden owner expiration

--- contracts/FlowerPower.sol
+++ contracts/FlowerPower.sol
@@ -11,16 +11,22 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IHammerFlower.sol";
 
+// const uint256 GARDEN_OWNER_TERM = 4672000; // 2 years of ethereum blocks (6400 per day)
+
 contract FlowerPower is ERC20, Ownable {
   IHammerFlower internal _hammerFlowerAddress;
+  uint256 internal _gardenOwnerExpiration;
 
   constructor(address hammerFlowerAddress) ERC20("FlowerPower", "FPOWER") {
+    // TODO: Set the garden owner expiration deadline based on the current block timestamp
     _hammerFlowerAddress = IHammerFlower(hammerFlowerAddress);
   }
 
   function balanceOf(address account) public view override returns (uint256) {
-    // TODO: Make this power go away in a term of a couple years (use the multiplier)
+    // TODO: Make this power go away in a term of two years
     if (account == owner()) {
+      // uint multiplier = (_gardenOwnerExpiration - now) / GARDEN_OWNER_TERM;
+      // return totalSupply() * multiplier;
       return totalSupply() * 1;
     }
     return _hammerFlowerAddress.powerBalanceOf(account);
